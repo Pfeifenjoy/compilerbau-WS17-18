65c582c63d58f8f320b89e4b7a2b58bd36f22989
NE4Y <slindner1996@gmail.com>
2017-12-14 14:02:05 +0100

    update meeting

--- project/test/unitTests.hs
+++ project/test/unitTests.hs
@@ -1,7 +1,17 @@
-module UnitTests where
+module Main where
 
 import Test.HUnit
-import 
+import Token
+import Lexer
 
 
+test1 :: Test
+test1 = TestCase (assertEqual "Equal-Assert" [Token.INTEGER] (Lexer.lex "test"))
 
+tests :: Test
+tests = TestList [TestLabel "test1" test1]
+ 
+main = do 
+    runTestTT tests
+
+         
