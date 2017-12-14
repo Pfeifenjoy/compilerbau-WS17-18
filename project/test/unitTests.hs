1a18949ee0fdefba750d476948c3daf66eafc567
Arwed Mett <arwed.mett@googlemail.com>
2017-12-14 18:59:31 +0100

    fixed tests

--- project/test/unitTests.hs
+++ project/test/unitTests.hs
@@ -1,12 +1,12 @@
 module Main where
 
 import Test.HUnit
-import Token
+import Lexer.Token
 import Lexer
 
 
 test1 :: Test
-test1 = TestCase (assertEqual "Equal-Assert" [Token.INTEGER] (Lexer.lex "test"))
+test1 = TestCase (assertEqual "Equal-Assert" [Lexer.Token.INT] (Lexer.lex "int"))
 
 tests :: Test
 tests = TestList [TestLabel "test1" test1]
