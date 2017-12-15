881549eaf232e21aacb9eefcddfdbc7d2896607a
NE4Y <slindner1996@gmail.com>
2017-12-15 13:07:53 +0100

    Update testsuite

--- project/test/unitTests.hs
+++ project/test/unitTests.hs
@@ -1,17 +1,33 @@
-module Main where
+module Tester where
 
 import Test.HUnit
 import Lexer.Token
 import Lexer
 
+data TestUnit = LexerUnit String [Token] [Token] -- Name, InputFile, CompareFile
 
-test1 :: Test
-test1 = TestCase (assertEqual "Equal-Assert" [Lexer.Token.INT] (Lexer.lex "int"))
+outPut :: Bool -> String -> String
+outPut cond name = if cond
+                       then concat ["\x1b[32m", "Lex: [", name, "] passed", "\x1b[0m"]
+                       else concat ["\x1b[35m", "Lex: [", name, "] failed", "\x1b[0m"]
 
-tests :: Test
-tests = TestList [TestLabel "test1" test1]
+runTest :: TestUnit -> String
+runTest (LexerUnit name a b) = outPut (a == b) name
+
+runTests :: [TestUnit] -> [String]
+runTests a = map runTest a
+
+firstTokenTest :: TestUnit
+firstTokenTest = LexerUnit "Empty Class" [Lexer.Token.INT, Lexer.Token.IDENTIFIER "i", Lexer.Token.SEMICOLON] (Lexer.lex "int i;")
+
+firstTokenTest2 :: TestUnit
+firstTokenTest2 = LexerUnit "Identifier" [Lexer.Token.INT, Lexer.Token.IDENTIFIER "k", Lexer.Token.SEMICOLON] (Lexer.lex "int i;")
+
+tests :: [TestUnit]
+tests = [firstTokenTest, firstTokenTest2]
  
-main = do 
-    runTestTT tests
+main = do
+        mapM_ putStrLn (runTests tests)
+ 
 
          
