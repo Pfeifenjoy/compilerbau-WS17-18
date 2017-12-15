aba9edb40e7caf6015e59ec08852a6d14d7b0983
NE4Y <slindner1996@gmail.com>
2017-12-15 20:55:25 +0100

    Add more testsuites and simplify LexerUnit-Tests

--- project/test/Arithmetic/Class.java
+++ project/test/Arithmetic/Class.java
@@ -1,5 +1,10 @@
 class Arithmetic {
 	int i = 5 + 3 * 2 - 1 % 3 / 1;
+	i += 3;
+	i -= 3;
+	i *= 3;
+	i /= 3;
+	i %= 3;
 	i++;
 	i--;
 }
--- project/test/unitTests.hs
+++ project/test/unitTests.hs
@@ -17,30 +17,33 @@ import Arithmetic.Steps
 import LogicOperations.Steps
 import BitWiseOperation.Steps
 
-data TestUnit = LexerUnit String [Token] [Token] -- TestName, TestTokens, FileTokens
+data TestUnit = LexerUnit String [Token] -- TestName, TestTokens
                 deriving(Eq, Show)
 
 getLexerName :: TestUnit -> String
-getLexerName (LexerUnit name testToken realToken) = name
+getLexerName (LexerUnit name testToken) = name
 
 getLexerTestToken :: TestUnit -> [Token]
-getLexerTestToken (LexerUnit name testToken realToken) = testToken
+getLexerTestToken (LexerUnit name testToken) = testToken
 
 getLexerRealToken :: TestUnit -> [Token]
-getLexerRealToken (LexerUnit name testToken realToken) = realToken
+getLexerRealToken (LexerUnit name testToken) = Lexer.lex (concat ["./test/", name, "/Class.java"])
 
 runTest :: TestUnit -> String
-runTest (LexerUnit name a b) = if a == b
+runTest (LexerUnit name a) = let b = Lexer.lex (concat ["./test/", name, "/Class.java"])
+                             in 
+                             if a == b
                                then concat ["\x1b[32m", "Lexer: [", name, "] passed", "\x1b[0m"]
                                else concat ["\x1b[35m", "Lexer: [", name, "] failed", "\x1b[0m", 
                                            "\n\t \x1b[36m expected: \x1b[0m ", (show a), 
                                            "\n\t \x1b[36m got: \x1b[0m ", (show b),
-                                           "\n\t \x1b[36m difference: \x1b[0m", (show (a \\ b))]                                        
+                                           "\n\t \x1b[36m difference: \x1b[0m", (show (nub (foldr (++) [] [a \\ b, b \\a])))]                                        
 runTests :: [TestUnit] -> [String]
 runTests a = map runTest a
 
+-- checks whether a test is a success or not
 isSuccess :: TestUnit -> Bool
-isSuccess (LexerUnit name a b) = a == b
+isSuccess (LexerUnit name a) = a == (Lexer.lex (concat ["./test/", name, "/Class.java"]))
 
 numberOfSuccess :: [TestUnit] -> Int
 numberOfSuccess a = sum (map fromEnum (map isSuccess a))
@@ -58,18 +61,18 @@ tokenCovering a = length (nub (map (\a -> skipParameter a) (foldr (++) [] (map (
                       
 
 lexTests :: [TestUnit]
-lexTests = [(LexerUnit "EmptyClass" emptyTokens (Lexer.lex "./test/EmptyClass/Class.java")),
-            (LexerUnit "InstanzVariable" instanzVariableTokens (Lexer.lex "./test/InstanzVariable/Class.java")),
-            (LexerUnit "ClassAssign" classAssignTokens (Lexer.lex "./test/ClassAssign/Class.java")),
-            (LexerUnit "ClassMethods" classMethodTokens (Lexer.lex "./test/ClassMethods/Class.java")),
-            (LexerUnit "ForLoop" forLoopTokens (Lexer.lex "./test/ForLoop/Class.java")),
-            (LexerUnit "LocalVariable" localVariableTokens (Lexer.lex "./test/LocalVariable/Class.java")),
-            (LexerUnit "SimpleIf" simpleIfTokens (Lexer.lex "./test/SimpleIf/Class.java")),
-            (LexerUnit "WhileLoop" whileLoopTokens (Lexer.lex "./test/WhileLoop/Class.java")),
-            (LexerUnit "WhileLoopCondition" whileLoopConditionTokens (Lexer.lex "./test/WhileLoopCondition/Class.java")),
-            (LexerUnit "Arithmetic" arithmeticTokens (Lexer.lex "./test/Arithmetic/Class.java")),
-            (LexerUnit "LogicOperations" logicOperationsTokens (Lexer.lex "./test/LogicOperations/Class.java")),
-            (LexerUnit "BitWiseOperation" bitWiseOperationTokens (Lexer.lex "./test/BitWiseOperation/Class.java"))
+lexTests = [(LexerUnit "EmptyClass" emptyTokens),
+            (LexerUnit "InstanzVariable" instanzVariableTokens),
+            (LexerUnit "ClassAssign" classAssignTokens),
+            (LexerUnit "ClassMethods" classMethodTokens),
+            (LexerUnit "ForLoop" forLoopTokens),
+            (LexerUnit "LocalVariable" localVariableTokens),
+            (LexerUnit "SimpleIf" simpleIfTokens),
+            (LexerUnit "WhileLoop" whileLoopTokens),
+            (LexerUnit "WhileLoopCondition" whileLoopConditionTokens),
+            (LexerUnit "Arithmetic" arithmeticTokens),
+            (LexerUnit "LogicOperations" logicOperationsTokens),
+            (LexerUnit "BitWiseOperation" bitWiseOperationTokens)
            ]
 
 main = do
@@ -77,6 +80,7 @@ main = do
        putStrLn ("\x1b[36mTokencoverage: " ++ show (tokenCovering lexTests) ++ " out of " ++ (show (length Lexer.Token.all_tokens)) ++ " \x1b[0m ")
        putStrLn ""
        putStrLn ("\x1b[36m" ++ (show (numberOfSuccess lexTests)) ++ " out of " ++ (show (length lexTests)) ++ " Tests passed \x1b[0m ")
+
        
  
 
