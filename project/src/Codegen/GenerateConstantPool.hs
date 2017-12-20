6b9d5024e4dddf81a7d7d956dad39970601922db
Florian Engel <floriaengel39@gmail.com>
2017-12-20 22:53:45 +0100

    Add function to get Constant for field variables
    Also change module to generate all constants for all CPInfos

--- project/src/Codegen/GenerateConstantPool.hs
+++ project/src/Codegen/GenerateConstantPool.hs
@@ -11,31 +11,51 @@ import Data.HashMap.Lazy ((!))
 Use this module to insert a constant in the constant pool
 
 -}
+-- | insert a class in the constant pool
+generateClass hm str index = undefined
 
--- | inserts a string in the constant pool
-generateUTF8 :: CPInfos -- ^ current constant pool
-             -> String -- ^ string to insert in the constant pool
-             -> Word8 -- ^ current highest index in constant pool
-             -> (CPInfos -- ^ new constant pool
-                ,Word8 -- ^ location of String in constant pool
-                ,Word8) -- ^ new highest index in constant pool 
-generateUTF8 hm str index = (newHM , newHM ! utf8info, newIndex)
+-- | insert a field variable in the constant pool
+generateFieldRef :: CPInfos -- ^ current constant pool
+                    -> Expr -- ^ field to insert in constant pool
+                    -> Word8 -- ^ current highest index in constant pool
+                    -> (CPInfos -- ^ new constant pool
+                       ,Word8 -- ^ location of field in constant pool
+                       ,Word8) -- ^ new highest index in constant pool 
+generateFieldRef hm (TypedExpr (LocalOrFieldVar name) typ) index 
+  = (newHM
+    , newHM ! fieldRefInfo
+    , newIndex
+    )
   where
-    utf8info = Utf8Info { tamCp = length str
-                        , cadCp = str
-                        , desc  = ""
-                        }
-    newHM = HM.insert utf8info (index+1) hm
-    newIndex = max (newHM ! utf8info) index
+    fieldRefInfo = FieldRefInfo { indexNameCp        = indexName
+                                , indexNameandtypeCp = indexNameType
+                                , desc               = ""
+                                } 
+    newHM = HM.insert fieldRefInfo (newIndex+1) hm''
+    (hm',indexName,maxIndex') = generateClass hm name index
+    (hm'',indexNameType,maxIndex'') = generateNameAndType hm name typ index
+    newIndex = maximum [index, newHM ! fieldRefInfo 
+                       , maxIndex', maxIndex''
+                       ]
 
--- | inserts a int in the constant pool
-generateInt :: CPInfos -- ^ current constant pool
-            -> Int -- ^ Int to insert in the constant pool
-            -> Word8 -- ^ current highest index in constant pool
-            -> (CPInfos -- ^ new constant pool
-               ,Word8 -- ^ location of String in constant pool
-               ,Word8) -- ^ new highest index in constant pool 
-generateInt hm int index = (newHM , newHM ! intInfo, newIndex)
+-- | insert a method in the constant pool
+generateStmtMehodRef hm (TypedStmtExpr (MethodCall expr str exprs) typ) index 
+   = undefined
+
+-- | insert a interface in the constant pool
+generateInterfaceMethodRef = undefined
+
+-- | insert a string in the constant pool
+generateString = undefined
+
+-- | inserts a integer in the constant pool
+generateInteger :: CPInfos -- ^ current constant pool
+                -> Int -- ^ Int to insert in the constant pool
+                -> Word8 -- ^ current highest index in constant pool
+                -> (CPInfos -- ^ new constant pool
+                   ,Word8 -- ^ location of int in constant pool
+                   ,Word8) -- ^ new highest index in constant pool 
+generateInteger hm int index = (newHM , newHM ! intInfo, newIndex)
   where
     intInfo = IntegerInfo { numiCp = int 
                            , desc  = ""
@@ -43,25 +63,24 @@ generateInt hm int index = (newHM , newHM ! intInfo, newIndex)
     newHM = HM.insert intInfo (index+1) hm
     newIndex = max (newHM ! intInfo) index
 
--- | inserts all constants from a given Expression in the constant pool
-generateExpr :: CPInfos -- ^ current constant pool
-             -> Expr 
-             -> Word8 -- ^ current highest index in constant pool
-             -> (CPInfos -- ^ new constant pool
-                ,Word8 -- ^ location of String in constant pool
-                ,Word8) -- ^ new highest index in constant pool 
-generateExpr hm This index = (newHM ,newHM ! clInfo, newIndex)
-  where
-    clInfo = ClassInfo { indexCp = indexName 
-                       , desc = ""
-                       }
-    newHM = HM.insert clInfo (index+1) hm'
-    (hm',indexName,maxIndex) = generateUTF8 hm "This" index
-    newIndex = maximum [index, newHM ! clInfo, maxIndex]
-                                           
--- generate Super 
--- variables
-generateExpr hm (TypedExpr (LocalOrFieldVar name) typ) index 
+-- | insert a float in the constant pool
+generateFloat = undefined
+
+-- | insert a long in the constant pool
+generateLong = undefined
+
+-- | insert a double in the constant pool
+generateDouble = undefined
+
+-- | insert name and type
+generateNameAndType :: CPInfos -- ^ current constant pool
+                    -> String -- ^ name to insert in constant pool 
+                    -> Type -- ^ type to insert in constant pool 
+                    -> Word8 -- ^ current highest index in constant pool
+                    -> (CPInfos -- ^ new constant pool
+                       ,Word8 -- ^ location of int in constant pool
+                       ,Word8) -- ^ new highest index in constant pool 
+generateNameAndType hm name typ index 
   = (newHM
     , newHM ! nameTypeInfo
     , newIndex
@@ -79,35 +98,18 @@ generateExpr hm (TypedExpr (LocalOrFieldVar name) typ) index
                        , maxIndex', maxIndex''
                        ]
 
-generateExpr hm (InstVar expr name) index = undefined 
-generateExpr hm (InstanceOf expr typ) index = undefined 
-    --literals
-generateExpr hm (BooleanLiteral bool) index = undefined 
-    -- | ByteLiteral Int8
-generateExpr hm (CharLiteral char) index = undefined 
-generateExpr hm (IntegerLiteral int32) index = undefined 
-    -- | LongLiteral Int64
-    -- | FloatLiteral Float
-    -- | DoubleLiteral Double
-    -- | StringLiteral String
-generateExpr hm JNull index = undefined 
-    -- other
-generateExpr hm (StmtExprExpr stmtExpr) index = undefined 
-generateExpr hm (TypedExpr expr _ ) index = undefined 
-generateExpt hm _ index = (hm,index)
-
-
-generateStmt :: CPInfos -> Stmt -> Word8 -> (CPInfos,Word8)
-generateStmt hm (LocalVarDecls variableDecls) index = undefined 
-generateStmt hm (StmtExprStmt stmtExpr) index = undefined 
-generateStmt hm (TypedStmt stmt typ) index = undefined 
-
-generateStmtVariableDecl :: CPInfos -> VariableDecl -> Word8 -> (CPInfos,Word8)
-generateStmtVariableDecl hm (VariableDecl name typ final (Just expr)) index = undefined 
-generateStmtVariableDecl hm (VariableDecl name typ final Nothing    ) index = undefined 
-
-generateStmtExpr :: CPInfos -> StmtExpr -> Word8 -> (CPInfos,Word8)
-generateStmtExpr hm (Assign expr1 expr2) index = undefined 
-generateStmtExpr hm (New typ argExprs) index = undefined 
-generateStmtExpr hm (MethodCall expr str exprs) index = undefined 
-generateStmtExpr hm (TypedStmtExpr stmtExpr typ) index = undefined 
+-- | inserts a utf8 in the constant pool
+generateUTF8 :: CPInfos -- ^ current constant pool
+             -> String -- ^ string to insert in the constant pool
+             -> Word8 -- ^ current highest index in constant pool
+             -> (CPInfos -- ^ new constant pool
+                ,Word8 -- ^ location of String in constant pool
+                ,Word8) -- ^ new highest index in constant pool 
+generateUTF8 hm str index = (newHM , newHM ! utf8info, newIndex)
+  where
+    utf8info = Utf8Info { tamCp = length str
+                        , cadCp = str
+                        , desc  = ""
+                        }
+    newHM = HM.insert utf8info (index+1) hm
+    newIndex = max (newHM ! utf8info) index
